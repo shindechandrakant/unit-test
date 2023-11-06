ea86df045f2f35d03f8322a138c475a222d77450
Chandrakant Shinde <chandrakantshinde7348@gmail.com>
2023-11-06 22:59:21 +0530

    Added 2 unit tests for async function

--- test/asynchAPI.test.js
+++ test/asynchAPI.test.js
@@ -3,6 +3,28 @@ const fetchData = require("../asynchAPI.js");
 
 
 describe("Test Async API Calls", () => {
+    
+    test("1. Test Async Code using Async/Await", async () => {
+
+        let response = JSON.stringify({
+            name: "Chandrakant Shinde",
+            age: 24,
+            email: "chandrakant@chandrakant.dev",
+          });
+        const data = await fetchData();
+        expect(data).toEqual(response);
+    });
+
+    test("1. Test Async Code using expect", async () => {
+
+        let response = JSON.stringify({
+            name: "Chandrakant Shinde",
+            age: 24,
+            email: "chandrakant@chandrakant.dev",
+          });
+        const data = fetchData();
+        expect(data).resolves.toEqual(response);
+    });
 
 });
 
